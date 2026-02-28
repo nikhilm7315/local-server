aa6618a608c98512607e025f4cacd5d2477fbb62
nikhim7315 <nikhilmishra7315@gmail.com>
2026-02-28 23:05:05 +0530

    created

---
+++ index.js
@@ -0,0 +1,10 @@
+const { log } = require("console");
+const http = require("http");
+
+const myServer = http.createServer((req, res) => {
+    console.log("New req. received");
+    res.end("Hello From Server");
+});
+
+
+myServer.listen(8000, () => console.log("Server Started!"));
